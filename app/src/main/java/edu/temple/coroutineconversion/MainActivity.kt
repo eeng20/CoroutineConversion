d58ca1f1113f452be0b66b516635289565772617
eeng20 <tul40997@temple.edu>
2024-10-31 14:49:05 -0400

    replaced thread with coroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,11 +7,16 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    //TODO (Refactor to replace Thread code with coroutines)
+    //TODO (Refactor to replace Thread code with coroutines)    
 
     private val cakeImageView: ImageView by lazy {
         findViewById(R.id.imageView)
@@ -33,12 +38,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+                CoroutineScope(Job() + Dispatchers.IO).launch{
+                    repeat(100) {
+                        handler.sendEmptyMessage(it)
+                        delay(40)
+                    }
+                }.start()
         }
     }
 }
